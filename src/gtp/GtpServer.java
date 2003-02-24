e41f2c2664d7cd2b4ce1bd487d86a968fe44b797
Markus Enzenberger <enz@users.sourceforge.net>
2003-02-24 22:14:01 +0000

    Implement asynchronous handling of abort command

--- src/gtp/GtpServer.java
+++ src/gtp/GtpServer.java
@@ -6,22 +6,33 @@
 package gtp;
 
 import java.io.*;
+import java.util.*;
 import utils.*;
 
 //-----------------------------------------------------------------------------
 
-class ReadThread
-    extends Thread
+class Command
 {
-    public ReadThread(InputStream in)
+    boolean m_hasId;
+    
+    int m_id;
+    
+    String m_command;
+}
+
+class ReadThread extends Thread
+{
+    public ReadThread(InputStream in, PrintStream out)
     {
         m_in = new BufferedReader(new InputStreamReader(in));
+        m_out = out;
     }
 
-    public String getLine()
+    public Command getCommand()
     {        
         synchronized (this)
         {
+            answerAbortCommands();
             notifyAll();
             try
             {
@@ -31,9 +42,9 @@ class ReadThread
             {
                 System.err.println("Interrupted");
             }
-            assert(m_receivedLine);
-            m_receivedLine = false;
-            return m_line;
+            Command result = m_command;
+            m_command = null;
+            return result;
         }
     }
 
@@ -45,16 +56,22 @@ class ReadThread
             {
                 while (! m_quit)
                 {
-                    assert(! m_receivedLine);
-                    m_line = m_in.readLine();
-                    if (m_line == null)
+                    String line;
+                    line = m_in.readLine();
+                    if (line == null)
                         return;
-                    m_line = m_line.trim();
-                    if (m_line.equals("") || m_line.charAt(0) == '#')
+                    line = line.trim();
+                    if (line.equals("") || line.charAt(0) == '#')
                         continue;
-                    m_receivedLine = true;
-                    notifyAll();
-                    wait();
+                    Command cmd = parseLine(line);
+                    if (cmd.m_command.trim().toLowerCase().equals("abort"))
+                        handleAbort(cmd);
+                    else
+                    {
+                        m_command = cmd;
+                        notifyAll();
+                        wait();
+                    }
                 }
             }
         }
@@ -78,53 +95,31 @@ class ReadThread
 
     private boolean m_quit = false;
 
-    private boolean m_receivedLine = false;
-
     private BufferedReader m_in;
 
-    private String m_line;
-}
-
-public abstract class GtpServer
-{
-    public GtpServer(InputStream in, OutputStream out)
-    {
-        m_out = new PrintStream(out);
-        m_in = in;
-    }
-
-    public abstract boolean handleCommand(String command,
-                                          StringBuffer response);
-
-    public void mainLoop() throws IOException
-    {
-        m_quit = false;
-        ReadThread readThread = new ReadThread(m_in);
-        readThread.start();
-        while (! m_quit)
-        {
-            String line = readThread.getLine();
-            parseLine(line);
-        }
-        readThread.quit();
-    }
-
-    protected void setQuit()
-    {
-        m_quit = true;
-    }
-
-    private boolean m_commandHadId;
-
-    private boolean m_quit;
-
-    private int m_commandId;
-
-    private InputStream m_in;
+    private Command m_command;
 
     private PrintStream m_out;
 
-    public synchronized void parseLine(String line)
+    private Vector m_abortCmds = new Vector();
+
+    private void answerAbortCommands()
+    {
+        for (int i = 0; i < m_abortCmds.size(); ++i)
+        {
+            Command cmd = (Command)m_abortCmds.get(i);
+            GtpServer.respond(m_out, true, cmd.m_hasId, cmd.m_id, "");
+        }
+        m_abortCmds.clear();
+    }
+
+    private void handleAbort(Command cmd)
+    {        
+        m_abortCmds.add(cmd);
+        System.err.println("aborted. FIXME: notify GtpServer");
+    }
+
+    private Command parseLine(String line)
     {
         assert(! line.trim().equals(""));
         int len = line.length();
@@ -152,30 +147,84 @@ public abstract class GtpServer
         String[] array = StringUtils.split(buffer.toString(), ' ');
         assert(array.length > 0);
         String command = buffer.toString();
+        Command result = new Command();
+        result.m_hasId = false;
+        result.m_command = command;
         try
         {
-            m_commandId = Integer.parseInt(array[0]);
-            m_commandHadId = true;
-            command = buffer.substring(array[0].length());
+            result.m_hasId = true;
+            result.m_id = Integer.parseInt(array[0]);
+            result.m_command = buffer.substring(array[0].length());
         }
         catch (NumberFormatException e)
         {
-            m_commandHadId = false;
+            result.m_hasId = false;
         }
-        StringBuffer response = new StringBuffer();
-        boolean status = handleCommand(command.trim(), response);
+        return result;
+    }
+}
+
+public abstract class GtpServer
+{
+    public GtpServer(InputStream in, OutputStream out)
+    {
+        m_out = new PrintStream(out);
+        m_in = in;
+    }
+
+    public abstract boolean handleCommand(String command,
+                                          StringBuffer response);
+
+    public void mainLoop() throws IOException
+    {
+        m_quit = false;
+        ReadThread readThread = new ReadThread(m_in, m_out);
+        readThread.start();
+        while (! m_quit)
+        {
+            Command command = readThread.getCommand();
+            sendResponse(command);
+        }
+        readThread.quit();
+    }
+
+    public static void respond(PrintStream out, boolean status, boolean hasId,
+                               int id, String response)
+    {
         if (status)
-            m_out.print('=');
+            out.print('=');
         else
-            m_out.print('?');
-        if (m_commandHadId)
-            m_out.print(m_commandId);
-        m_out.print(' ');
+            out.print('?');
+        if (hasId)
+            out.print(id);
+        out.print(' ');
+        out.print(response);
         if (response.length() == 0
             || response.charAt(response.length() - 1) != '\n')
-            response.append('\n');
-        response.append('\n');
-        m_out.print(response.toString());
+            out.print('\n');
+        out.print('\n');
+    }
+
+    protected void setQuit()
+    {
+        m_quit = true;
+    }
+
+    private boolean m_commandHadId;
+
+    private boolean m_quit;
+
+    private int m_commandId;
+
+    private InputStream m_in;
+
+    private PrintStream m_out;
+
+    private void sendResponse(Command cmd)
+    {
+        StringBuffer response = new StringBuffer();
+        boolean status = handleCommand(cmd.m_command.trim(), response);
+        respond(m_out, status, cmd.m_hasId, cmd.m_id, response.toString());
     }
 }
 
