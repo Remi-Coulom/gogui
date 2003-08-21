6748b55fb17c601ec521d0042c8125cb8820c0c3
Markus Enzenberger <enz@users.sourceforge.net>
2003-08-21 23:05:02 +0000

    Add function getRelativePath()

--- src/utils/FileUtils.java
+++ src/utils/FileUtils.java
@@ -22,6 +22,36 @@ public class FileUtils
         return ext;
     }
 
+    /** Relative path from file1 to file2. */
+    public static String getRelativePath(File file1, File file2)
+        throws IOException
+    {
+        file1 = file1.getCanonicalFile();
+        file2 = file2.getCanonicalFile();
+        if (file1.isFile())
+            file1 = file1.getParentFile();
+        if (file2.isFile())
+            file2 = file2.getParentFile();
+        char sep = File.separatorChar;
+        String[] dir1 = StringUtils.split(file1.toString(), sep);
+        String[] dir2 = StringUtils.split(file2.toString(), sep);
+        int i = 0;
+        while (i < dir1.length && i < dir2.length && dir1[i].equals(dir2[i]))
+            ++i;
+        StringBuffer result = new StringBuffer();
+        for (int j = i; j < dir1.length; ++j)
+        {
+            result.append("..");
+            result.append(sep);
+        }
+        for (int j = i; j < dir2.length; ++j)
+        {
+            result.append(dir2[i]);
+            result.append(sep);
+        }
+        return result.toString();
+    }
+
     /** Check for extension (case-insensitive) */
     public static boolean hasExtension(File f, String extension)
     {
