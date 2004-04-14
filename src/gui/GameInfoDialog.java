a74d76bd341751f10a05f6b72c71ad9354bc2810
Markus Enzenberger <enz@users.sourceforge.net>
2004-04-14 01:26:08 +0000

    Shorter window title

--- src/gui/GameInfoDialog.java
+++ src/gui/GameInfoDialog.java
@@ -22,7 +22,7 @@ public class GameInfoDialog
     {
         GameInfoDialog gameInfoDialog = new GameInfoDialog(gameInformation);
         JDialog dialog =
-            gameInfoDialog.createDialog(parent, "GoGui: Game Information");
+            gameInfoDialog.createDialog(parent, "GoGui: Game Info");
         boolean done = false;
         while (! done)
         {
