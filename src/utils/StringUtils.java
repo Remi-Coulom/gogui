69b5e72721f3b41efa6bfb12ee0908b8be1650db
Markus Enzenberger <enz@users.sourceforge.net>
2004-04-14 14:27:47 +0000

    Introduce replace again, because String.replaceAll is nt supported on all Java systems

--- src/utils/StringUtils.java
+++ src/utils/StringUtils.java
@@ -51,6 +51,49 @@ public class StringUtils
         return format;
     }
 
+    public static String replace(String s, String oldStr, String newStr)
+    {
+        StringBuffer buffer = new StringBuffer(s);
+        replace(buffer, oldStr, newStr);
+        return new String(buffer);
+    }
+    
+    public static void replace(StringBuffer buffer, String oldStr,
+                               String newStr)
+    {
+        int newLen = newStr.length();
+        int oldLen = oldStr.length();
+        int idx = 0;
+        while (true)
+        {
+            int i = buffer.toString().indexOf(oldStr, idx);
+            if (i < 0)
+                break;
+            buffer.replace(i, i + oldLen, newStr);
+            idx += newLen;
+        }
+    }
+
+    public static String[] split(String s, char separator)
+    {
+        int count = 1;
+        int pos = -1;
+        while ((pos = s.indexOf(separator, pos + 1)) >= 0)
+            ++count;
+        String result[] = new String[count];
+        pos = 0;
+        int newPos;
+        int i = 0;
+        while ((newPos = s.indexOf(separator, pos)) >= 0)
+        {
+            result[i] = s.substring(pos, newPos);
+            ++i;
+            pos = newPos + 1;
+        }
+        result[i] = s.substring(pos);
+        return result;
+    }
+
     /** Split string into words.
         Allows " for words containing whitespaces.
     */
@@ -90,26 +133,6 @@ public class StringUtils
         String result[] = new String[size];
         for (int i = 0; i < size; ++i)
             result[i] = (String)vector.get(i);
-        return result;
-    }
-
-    public static String[] split(String s, char separator)
-    {
-        int count = 1;
-        int pos = -1;
-        while ((pos = s.indexOf(separator, pos + 1)) >= 0)
-            ++count;
-        String result[] = new String[count];
-        pos = 0;
-        int newPos;
-        int i = 0;
-        while ((newPos = s.indexOf(separator, pos)) >= 0)
-        {
-            result[i] = s.substring(pos, newPos);
-            ++i;
-            pos = newPos + 1;
-        }
-        result[i] = s.substring(pos);
         return result;
     }
 }
