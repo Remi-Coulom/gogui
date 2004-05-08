d36ffcb3f90a6a3f2134893a17558c66fa60bdfb
Markus Enzenberger <enz@users.sourceforge.net>
2004-05-08 18:04:59 +0000

    Construct StringBuffer with higher initial capacity

--- src/gtp/Gtp.java
+++ src/gtp/Gtp.java
@@ -563,7 +563,7 @@ public final class Gtp
             {
                 int size = 1024;
                 char[] buffer = new char[size];
-                StringBuffer stringBuffer = new StringBuffer(256);
+                StringBuffer stringBuffer = new StringBuffer(1024);
                 while (true)
                 {
                     int n = m_err.read(buffer, 0, size);
