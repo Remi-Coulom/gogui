42b38499136684d7c09e4340c4bbe2ef1fe6faf9
Markus Enzenberger <enz@users.sourceforge.net>
2004-11-19 00:47:48 +0000

    GtpDummy: factor out class Main

--- src/gtpdummy/GtpDummy.java
+++ src/gtpdummy/GtpDummy.java
@@ -105,53 +105,6 @@ public class GtpDummy
         m_thread.interrupt();
     }
 
-    public static void main(String[] args)
-    {
-        try
-        {
-            String options[] = {
-                "config:",
-                "help",
-                "log:",
-                "version"
-            };
-            Options opt = new Options(args, options);
-            opt.handleConfigOption();
-            if (opt.isSet("help"))
-            {
-                String helpText =
-                    "Usage: java -jar gtpdummy.jar [options]\n" +
-                    "\n" +
-                    "-config       config file\n" +
-                    "-help         display this help and exit\n" +
-                    "-log file     log GTP stream to file\n" +
-                    "-version      print version and exit\n";
-                System.out.print(helpText);
-                return;
-            }
-            if (opt.isSet("version"))
-            {
-                System.out.println("GtpDummy " + Version.get());
-                return;
-            }
-            PrintStream log = null;
-            if (opt.isSet("log"))
-            {
-                File file = new File(opt.getString("log"));
-                log = new PrintStream(new FileOutputStream(file));
-            }
-            GtpDummy gtpDummy = new GtpDummy(System.in, System.out, log);
-            gtpDummy.mainLoop();
-            if (log != null)
-                log.close();
-        }
-        catch (Throwable t)
-        {
-            StringUtils.printException(t);
-            System.exit(-1);
-        }
-    }
-    
     private boolean m_nextResponseFixed;
 
     private boolean m_nextStatus;
---
+++ src/gtpdummy/Main.java
@@ -0,0 +1,67 @@
+//----------------------------------------------------------------------------
+// $Id$
+// $Source$
+//----------------------------------------------------------------------------
+
+package gtpdummy;
+
+import java.io.*;
+import go.*;
+import gtp.*;
+import utils.Options;
+import utils.StringUtils;
+import version.*;
+
+//----------------------------------------------------------------------------
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            String options[] = {
+                "config:",
+                "help",
+                "log:",
+                "version"
+            };
+            Options opt = new Options(args, options);
+            opt.handleConfigOption();
+            if (opt.isSet("help"))
+            {
+                String helpText =
+                    "Usage: java -jar gtpdummy.jar [options]\n" +
+                    "\n" +
+                    "-config       config file\n" +
+                    "-help         display this help and exit\n" +
+                    "-log file     log GTP stream to file\n" +
+                    "-version      print version and exit\n";
+                System.out.print(helpText);
+                return;
+            }
+            if (opt.isSet("version"))
+            {
+                System.out.println("GtpDummy " + Version.get());
+                return;
+            }
+            PrintStream log = null;
+            if (opt.isSet("log"))
+            {
+                File file = new File(opt.getString("log"));
+                log = new PrintStream(new FileOutputStream(file));
+            }
+            GtpDummy gtpDummy = new GtpDummy(System.in, System.out, log);
+            gtpDummy.mainLoop();
+            if (log != null)
+                log.close();
+        }
+        catch (Throwable t)
+        {
+            StringUtils.printException(t);
+            System.exit(-1);
+        }
+    }
+}
+
+//----------------------------------------------------------------------------
