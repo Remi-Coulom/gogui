0d16156ba3123980238ec8a4e3d63184f4a611c3
Markus Enzenberger <enz@users.sourceforge.net>
2004-11-19 00:36:04 +0000

    GtpTerminal: factor out class Main

--- src/gtpterminal/GtpTerminal.java
+++ src/gtpterminal/GtpTerminal.java
@@ -37,49 +37,6 @@ public class GtpTerminal
         m_gtp.waitForExit();
     }
 
-    public static void main(String[] args)
-    {
-        try
-        {
-            String options[] = {
-                "config:",
-                "help",
-                "size:",
-                "verbose",
-                "version"
-            };
-            Options opt = new Options(args, options);
-            opt.handleConfigOption();
-            if (opt.isSet("help"))
-            {
-                printUsage(System.out);
-                return;
-            }
-            if (opt.isSet("version"))
-            {
-                System.out.println("GtpTerminal " + Version.get());
-                return;
-            }
-            int size = opt.getInteger("size", 19, 1);
-            boolean verbose = opt.isSet("verbose");
-            Vector arguments = opt.getArguments();
-            if (arguments.size() != 1)
-            {
-                printUsage(System.err);
-                System.exit(-1);
-            }
-            String program = (String)arguments.get(0);
-            GtpTerminal gtpTerminal = new GtpTerminal(program, size, verbose);
-            gtpTerminal.mainLoop();
-            gtpTerminal.close();
-        }
-        catch (Throwable t)
-        {
-            StringUtils.printException(t);
-            System.exit(-1);
-        }
-    }
-
     public void mainLoop() throws IOException
     {
         newGame(m_board.getSize());
@@ -369,19 +326,6 @@ public class GtpTerminal
     private void printBoard()
     {
         BoardUtils.print(m_board, System.out);
-    }
-
-    private static void printUsage(PrintStream out)
-    {
-        String helpText =
-            "Usage: java -jar gtpterminal.jar program\n" +
-            "\n" +
-            "-config       config file\n" +
-            "-help         print help and exit\n" +
-            "-size n       board size (default 19)\n" +
-            "-verbose      print debug information\n" +
-            "-version      print version and exit\n";
-        out.print(helpText);
     }
 
     private void save(String[] cmdArray)
