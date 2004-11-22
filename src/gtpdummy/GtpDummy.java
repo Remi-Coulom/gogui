04662fed7e8b3ffca0e62e1fa1507d6042c70b95
Markus Enzenberger <enz@users.sourceforge.net>
2004-11-22 04:09:20 +0000

    Convert GtpDummy to GTP version 2

--- src/gtpdummy/GtpDummy.java
+++ src/gtpdummy/GtpDummy.java
@@ -39,8 +39,10 @@ public class GtpDummy
             response.append(m_nextResponse);
             m_nextResponseFixed = false;
         }
-        else if (cmd.equals("black"))
-            status = play(cmdArray, response);
+        else if (cmd.equals("boardsize"))
+            status = cmdBoardsize(cmdArray, response);
+        else if (cmd.equals("clear_board"))
+            status = cmdClearBoard(response);
         else if (cmd.equals("dummy_bwboard"))
             bwBoard(response);
         else if (cmd.equals("dummy_invalid"))
@@ -51,32 +53,25 @@ public class GtpDummy
             nextResponseFixed(cmd, cmdLine, false);
         else if (cmd.equals("dummy_next_success"))
             nextResponseFixed(cmd, cmdLine, true);
+        else if (cmd.equals("dummy_sleep"))
+            status = sleep(cmdArray, response);
         else if (cmd.equals("echo"))
             echo(cmdLine, response);
         else if (cmd.equals("echo_err"))
             echoErr(cmdLine);
-        else if (cmd.equals("genmove_black"))
-            status = genmove(response);
-        else if (cmd.equals("boardsize"))
-            status = boardsize(cmdArray, response);
+        else if (cmd.equals("genmove"))
+            status = cmdGenmove(response);
         else if (cmd.equals("gogui_interrupt"))
             ;
-        else if (cmd.equals("genmove_white"))
-            status = genmove(response);
         else if (cmd.equals("name"))
             response.append("GtpDummy");
+        else if (cmd.equals("play"))
+            status = cmdPlay(cmdArray, response);
         else if (cmd.equals("protocol_version"))
-            response.append("1");
-        else if (cmd.equals("dummy_sleep"))
-            status = sleep(cmdArray, response);
-        else if (cmd.equals("quit"))
-            ;
-        else if (cmd.equals("version"))
-            response.append(Version.get());
-        else if (cmd.equals("white"))
-            status = play(cmdArray, response);
-        else if (cmd.equals("help"))
-            response.append("black\n" +
+            response.append("2");
+        else if (cmd.equals("list_commands"))
+            response.append("boardsize\n" +
+                            "clear_board\n" +
                             "dummy_bwboard\n" +
                             "dummy_crash\n" +
                             "dummy_invalid\n" +
@@ -85,16 +80,18 @@ public class GtpDummy
                             "dummy_sleep\n" +
                             "echo\n" +
                             "echo_err\n" +
-                            "genmove_black\n" +
-                            "genmove_white\n" +
+                            "genmove\n" +
                             "gogui_interrupt\n" +
-                            "help\n" +
-                            "white\n" +
+                            "list_commands\n" +
                             "name\n" +
+                            "play\n" +
                             "protocol_version\n" +
                             "quit\n" +
-                            "version\n" +
-                            "white\n");
+                            "version\n");
+        else if (cmd.equals("version"))
+            response.append(Version.get());
+        else if (cmd.equals("quit"))
+            ;
         else
         {
             response.append("unknown command");
@@ -120,7 +117,18 @@ public class GtpDummy
 
     private Thread m_thread;
 
-    private boolean boardsize(String[] cmdArray, StringBuffer response)
+    private void bwBoard(StringBuffer response)
+    {        
+        response.append("\n");
+        for (int x = 0; x < m_size; ++x)
+        {
+            for (int y = 0; y < m_size; ++y)
+                response.append(Math.random() > 0.5 ? "B " : "W ");
+            response.append("\n");
+        }                    
+    }
+
+    private boolean cmdBoardsize(String[] cmdArray, StringBuffer response)
     {
         IntegerArgument argument = parseIntegerArgument(cmdArray, response);
         if (argument == null)
@@ -134,46 +142,13 @@ public class GtpDummy
         return true;
     }
 
-    private void bwBoard(StringBuffer response)
-    {        
-        response.append("\n");
-        for (int x = 0; x < m_size; ++x)
-        {
-            for (int y = 0; y < m_size; ++y)
-                response.append(Math.random() > 0.5 ? "B " : "W ");
-            response.append("\n");
-        }                    
-    }
-
-    private void cmdInvalid()
-    {        
-        printInvalidResponse("This is an invalid GTP response.\n" +
-                             "It does not start with a status character.\n");
-    }
-
-    private void crash()
-    {        
-        System.err.println("Aborting GtpDummy");
-        System.exit(-1);
-    }
-
-    private void echo(String cmdLine, StringBuffer response)
+    private boolean cmdClearBoard(StringBuffer response)
     {
-        int index = cmdLine.indexOf(" ");
-        if (index < 0)
-            return;
-        response.append(cmdLine.substring(index + 1));
+        initSize(m_size);
+        return true;
     }
 
-    private void echoErr(String cmdLine)
-    {
-        int index = cmdLine.indexOf(" ");
-        if (index < 0)
-            return;
-        System.err.println(cmdLine.substring(index + 1));
-    }
-
-    private boolean genmove(StringBuffer response)
+    private boolean cmdGenmove(StringBuffer response)
     {
         int numberPossibleMoves = 0;
         for (int x = 0; x < m_size; ++x)
@@ -200,6 +175,46 @@ public class GtpDummy
         return true;
     }
 
+    private void cmdInvalid()
+    {        
+        printInvalidResponse("This is an invalid GTP response.\n" +
+                             "It does not start with a status character.\n");
+    }
+
+    private boolean cmdPlay(String[] cmdArray, StringBuffer response)
+    {
+        ColorPointArgument argument =
+            parseColorPointArgument(cmdArray, response, m_size);
+        if (argument == null)
+            return false;
+        Point point = argument.m_point;
+        if (point != null)
+            m_alreadyPlayed[point.getX()][point.getY()] = true;
+        return true;
+    }
+
+    private void crash()
+    {        
+        System.err.println("Aborting GtpDummy");
+        System.exit(-1);
+    }
+
+    private void echo(String cmdLine, StringBuffer response)
+    {
+        int index = cmdLine.indexOf(" ");
+        if (index < 0)
+            return;
+        response.append(cmdLine.substring(index + 1));
+    }
+
+    private void echoErr(String cmdLine)
+    {
+        int index = cmdLine.indexOf(" ");
+        if (index < 0)
+            return;
+        System.err.println(cmdLine.substring(index + 1));
+    }
+
     private void initSize(int size)
     {
         m_alreadyPlayed = new boolean[size][size];
@@ -212,28 +227,6 @@ public class GtpDummy
         m_nextResponseFixed = true;
         m_nextStatus = nextStatus;
         m_nextResponse = cmdLine.substring(cmd.length()).trim();
-    }
-
-    private boolean play(String[] cmdArray, StringBuffer response)
-    {
-        if (cmdArray.length < 2)
-        {
-            response.append("Missing argument");
-            return false;
-        }
-        Point point = null;
-        try
-        {
-            point = GtpUtils.parsePoint(cmdArray[1], m_size);
-        }
-        catch (GtpError e)
-        {
-            response.append(e.getMessage());
-            return false;
-        }
-        if (point != null)
-            m_alreadyPlayed[point.getX()][point.getY()] = true;
-        return true;
     }
 
     private boolean sleep(String[] cmdArray, StringBuffer response)
