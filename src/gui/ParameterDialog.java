c81eb52620dbc6d5da6c13d8c0f5b328834f8bbe
Markus Enzenberger <enz@users.sourceforge.net>
2004-11-29 18:44:59 +0000

    Fix wrong escaping in pattern of last change

--- src/gui/ParameterDialog.java
+++ src/gui/ParameterDialog.java
@@ -193,7 +193,7 @@ public class ParameterDialog
             if (line.startsWith("[bool]"))
             {
                 type = BOOL;
-                line = line.replaceFirst("[bool]", "").trim();
+                line = line.replaceFirst("\\[bool\\]", "").trim();
             }
             int pos = line.indexOf(' ');
             String key;
