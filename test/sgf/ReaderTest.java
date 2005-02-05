84496d5d4fd5c0c08f1c28cd603490ff79ca90f5
Markus Enzenberger <enz@users.sourceforge.net>
2005-02-05 00:03:46 +0000

    Add tests for reading TimeSettings with sgf.Reader

--- test/sgf/ReaderTest.java
+++ test/sgf/ReaderTest.java
@@ -7,6 +7,9 @@ package sgf;
 
 import java.io.InputStream;
 import junit.framework.TestCase;
+import game.GameTree;
+import game.GameInformation;
+import game.TimeSettings;
 
 //----------------------------------------------------------------------------
 
@@ -16,6 +19,28 @@ public class ReaderTest
     public void testRead() throws Exception
     {
         readSgfFile("verbose-property-names.sgf", false, true);
+        checkTimeSettings("time-settings-1.sgf", 1800000, 60000, 5);
+    }
+
+    private Reader getReader(String name) throws Reader.SgfError, Exception
+    {
+        InputStream in = getClass().getResourceAsStream(name);
+        if (in == null)
+            throw new Exception("Resource " + name + " not found");
+        return new Reader(in, null, null, 0);
+    }
+
+    private void checkTimeSettings(String name, long preByoyomi, long byoyomi,
+                                   int byoyomiMoves) throws Exception
+    {
+        Reader reader = getReader(name);
+        GameTree gameTree = reader.getGameTree();
+        GameInformation gameInformation = gameTree.getGameInformation();
+        TimeSettings timeSettings = gameInformation.m_timeSettings;
+        assertNotNull(timeSettings);
+        assertEquals(timeSettings.getPreByoyomi(), preByoyomi);
+        assertEquals(timeSettings.getByoyomi(), byoyomi);
+        assertEquals(timeSettings.getByoyomiMoves(), byoyomiMoves);
     }
 
     private void readSgfFile(String name, boolean expectFailure,
@@ -23,10 +48,7 @@ public class ReaderTest
     {
         try
         {
-            InputStream in = getClass().getResourceAsStream(name);
-            if (in == null)
-                throw new Exception("Resource " + name + " not found");
-            Reader reader = new Reader(in, null, null, 0);
+            Reader reader = getReader(name);
             if (expectWarnings && reader.getWarnings() == null)
                 fail("Reading " + name + " should result in warnings");
             if (! expectWarnings && reader.getWarnings() != null)
