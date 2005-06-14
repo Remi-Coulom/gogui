298407f5eea6890f869cea9377bdd44a00454985
Markus Enzenberger <enz@users.sourceforge.net>
2005-06-14 17:27:01 +0000

    Add more tests

--- test/junit/src/net/sf/gogui/utils/OptionsTest.java
+++ test/junit/src/net/sf/gogui/utils/OptionsTest.java
@@ -15,14 +15,29 @@ public class OptionsTest
 {
     public void testBasic() throws ErrorMessage
     {
-        String specs[] = { "flag1", "value1:", "value2:", "flag2" };
-        String args[] = { "arg1", "-value1:", "42", "-flag2", "arg2" };
+        String specs[] = {
+            "flag1",
+            "value1:",
+            "value2:",
+            "flag2",
+            "value3:"
+        };
+        String args[] = {
+            "arg1",
+            "-value1:", "42",
+            "-flag2",
+            "-value3:", "-9223372036854775807",
+            "arg2"
+        };
         Options opt = new Options(args, specs);
         assertFalse(opt.isSet("flag1"));
         assertTrue(opt.isSet("flag2"));
         assertTrue(opt.isSet("value1"));
         assertFalse(opt.isSet("value2"));
+        assertEquals(opt.getString("value1"), "42");
         assertEquals(opt.getInteger("value1"), 42);
+        assertEquals(opt.getInteger("value2", -98), -98);
+        assertEquals(opt.getLong("value3"), -9223372036854775807L);
         Vector arguments = opt.getArguments();
         assertEquals(arguments.size(), 2);
         assertEquals(arguments.get(0), "arg1");
