2d44c9b2384cc9909d39cd6705d022777aae6e81
Markus Enzenberger <enz@users.sourceforge.net>
2005-06-16 01:38:50 +0000

    Add workarounds for incomplete JTextPane implementation of GCJ

--- src/net/sf/gogui/gui/Comment.java
+++ src/net/sf/gogui/gui/Comment.java
@@ -5,25 +5,18 @@
 
 package net.sf.gogui.gui;
 
+import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.KeyboardFocusManager;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.swing.JScrollPane;
-import javax.swing.JTextPane;
-import javax.swing.KeyStroke;
 import javax.swing.event.CaretEvent;
 import javax.swing.event.CaretListener;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.text.BadLocationException;
-import javax.swing.text.Style;
-import javax.swing.text.StyleConstants;
-import javax.swing.text.StyleContext;
-import javax.swing.text.StyledDocument;
 import net.sf.gogui.game.Node;
+import net.sf.gogui.gui.GuiTextPane;
 import net.sf.gogui.gui.GuiUtils;
 
 //----------------------------------------------------------------------------
@@ -45,14 +38,8 @@ public class Comment
     public Comment(Listener listener)
     {
         m_listener = listener;
-        m_textPane = new JTextPane();
-        StyledDocument doc = m_textPane.getStyledDocument();
-        StyleContext context = StyleContext.getDefaultStyleContext();
-        m_defaultStyle = context.getStyle(StyleContext.DEFAULT_STYLE);
-        Style styleMarked = doc.addStyle("marked", m_defaultStyle);
-        StyleConstants.setBackground(styleMarked,
-                                     java.awt.Color.decode("#38d878"));
-        StyleConstants.setForeground(styleMarked, java.awt.Color.white);
+        m_textPane = GuiTextPaneFactory.create();
+        m_textPane.addStyle("marked", Color.white, Color.decode("#38d878"));
         int fontSize = GuiUtils.getDefaultMonoFontSize();
         setPreferredSize(new Dimension(20 * fontSize, 10 * fontSize));
         m_textPane.getDocument().addDocumentListener(this);
@@ -62,30 +49,12 @@ public class Comment
                 {
                     if (m_listener == null)
                         return;
-                    int start = m_textPane.getSelectionStart();
-                    int end = m_textPane.getSelectionEnd();
-                    StyledDocument doc = m_textPane.getStyledDocument();
-                    try
-                    {
-                        String text = doc.getText(start, end - start);
-                        m_listener.textSelected(text);
-                    }
-                    catch (BadLocationException e)
-                    {
-                        assert(false);
-                    }   
+                    m_listener.textSelected(m_textPane.getSelection());
                 }
             };
         m_textPane.addCaretListener(caretListener);
-        Set forwardSet  = new HashSet();
-        forwardSet.add(KeyStroke.getKeyStroke("TAB"));
-        int forwardId = KeyboardFocusManager.FORWARD_TRAVERSAL_KEYS;
-        m_textPane.setFocusTraversalKeys(forwardId, forwardSet);
-        Set backwardSet  = new HashSet();
-        backwardSet.add(KeyStroke.getKeyStroke("shift TAB"));
-        int backwardId = KeyboardFocusManager.BACKWARD_TRAVERSAL_KEYS;
-        m_textPane.setFocusTraversalKeys(backwardId, backwardSet);
-        setViewportView(m_textPane);
+        m_textPane.setTabFocusKeys();
+        setViewportView(m_textPane.getComponent());
         setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
     }
@@ -102,7 +71,7 @@ public class Comment
 
     public String getSelectedText()
     {
-        return m_textPane.getSelectedText();
+        return m_textPane.getSelection();
     }
 
     public void insertUpdate(DocumentEvent e)
@@ -112,31 +81,7 @@ public class Comment
 
     public void markAll(Pattern pattern)
     {
-        StyledDocument doc = m_textPane.getStyledDocument();
-        try
-        {
-            CharSequence text = doc.getText(0, doc.getLength());
-            Matcher matcher = pattern.matcher(text);
-            boolean firstMatch = true;
-            while (matcher.find())
-            {
-                int start = matcher.start();
-                int end = matcher.end();
-                Style style = doc.getStyle("marked");
-                if (firstMatch)
-                {
-                    doc.setCharacterAttributes(0, doc.getLength(),
-                                               m_defaultStyle, true);
-                    m_textPane.setCaretPosition(start);
-                    firstMatch = false;
-                }
-                doc.setCharacterAttributes(start, end - start, style, true);
-            }
-        }
-        catch (BadLocationException e)
-        {
-            assert(false);
-        }
+        m_textPane.markAll(pattern);
     }
 
     public void removeUpdate(DocumentEvent e)
@@ -153,10 +98,7 @@ public class Comment
         // setText() generates a remove and insert event, and
         // we don't want to notify the listener about that yet.
         m_duringSetText = true;
-        StyledDocument doc = m_textPane.getStyledDocument();
-        doc.setCharacterAttributes(0, doc.getLength(), m_defaultStyle, true);
         m_textPane.setText(text);
-        m_textPane.setCaretPosition(0);
         m_duringSetText = false;
         copyContentToNode();
     }
@@ -168,13 +110,11 @@ public class Comment
     */
     private static final long serialVersionUID = 0L; // SUID
 
-    private final JTextPane m_textPane;
+    private final GuiTextPane m_textPane;
 
     private final Listener m_listener;
 
     private Node m_node;
-
-    private final Style m_defaultStyle;
 
     private void copyContentToNode()
     {
---
+++ src/net/sf/gogui/gui/GuiTextPane.java
@@ -0,0 +1,204 @@
+//----------------------------------------------------------------------------
+// $Id$
+// $Source$
+//----------------------------------------------------------------------------
+
+package net.sf.gogui.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.KeyboardFocusManager;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.swing.JComponent;
+import javax.swing.JTextArea;
+import javax.swing.JTextPane;
+import javax.swing.KeyStroke;
+import javax.swing.event.CaretListener;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyleContext;
+import javax.swing.text.StyledDocument;
+
+//----------------------------------------------------------------------------
+
+/** Component for displaying text.
+    This is a workaround for the incomplete implementation of JTextPane of
+    GCJ on FC4. If the system property "gogui.no-jtextpane" is set
+    an implementation of GuiTextPane based on JTextArea is used and styles
+    are ignored.
+*/
+public interface GuiTextPane
+{
+    public abstract void addCaretListener(CaretListener listener);
+
+    public abstract void addStyle(String name, Color foreground,
+                                  Color background);
+
+    public abstract JComponent getComponent();
+
+    public abstract Document getDocument();
+
+    public abstract String getSelection();
+
+    public abstract String getText();
+
+    public abstract void markAll(Pattern pattern);
+
+    public abstract void setTabFocusKeys();
+
+    public abstract void setText(String text);
+}
+
+//----------------------------------------------------------------------------
+
+/** Implementation of GuiTextPane using a JTextPane. */
+class GuiJTextPane
+    extends JTextPane
+    implements GuiTextPane
+{
+    public GuiJTextPane()
+    {
+    }
+
+    public void addStyle(String name, Color foreground, Color background)
+    {
+        StyledDocument doc = getStyledDocument();
+        StyleContext context = StyleContext.getDefaultStyleContext();
+        m_defaultStyle = context.getStyle(StyleContext.DEFAULT_STYLE);
+        Style style = doc.addStyle(name, m_defaultStyle);
+        StyleConstants.setForeground(style, foreground);
+        StyleConstants.setBackground(style, background);
+    }
+
+    public JComponent getComponent()
+    {
+        return this;
+    }
+
+    public String getSelection()
+    {
+        int start = getSelectionStart();
+        int end = getSelectionEnd();
+        StyledDocument doc = getStyledDocument();
+        try
+        {
+            return doc.getText(start, end - start);
+        }
+        catch (BadLocationException e)
+        {
+            assert(false);
+            return "";
+        }   
+    }
+
+    public void markAll(Pattern pattern)
+    {
+        StyledDocument doc = getStyledDocument();
+        try
+        {
+            CharSequence text = doc.getText(0, doc.getLength());
+            Matcher matcher = pattern.matcher(text);
+            boolean firstMatch = true;
+            while (matcher.find())
+            {
+                int start = matcher.start();
+                int end = matcher.end();
+                Style style = doc.getStyle("marked");
+                if (firstMatch)
+                {
+                    doc.setCharacterAttributes(0, doc.getLength(),
+                                               m_defaultStyle, true);
+                    setCaretPosition(start);
+                    firstMatch = false;
+                }
+                doc.setCharacterAttributes(start, end - start, style, true);
+            }
+        }
+        catch (BadLocationException e)
+        {
+            assert(false);
+        }
+    }
+
+    public void setTabFocusKeys()
+    {
+        Set forwardSet  = new HashSet();
+        forwardSet.add(KeyStroke.getKeyStroke("TAB"));
+        int forwardId = KeyboardFocusManager.FORWARD_TRAVERSAL_KEYS;
+        setFocusTraversalKeys(forwardId, forwardSet);
+        Set backwardSet  = new HashSet();
+        backwardSet.add(KeyStroke.getKeyStroke("shift TAB"));
+        int backwardId = KeyboardFocusManager.BACKWARD_TRAVERSAL_KEYS;
+        setFocusTraversalKeys(backwardId, backwardSet);
+    }
+
+    public void setText(String text)
+    {
+        StyledDocument doc = getStyledDocument();
+        doc.setCharacterAttributes(0, doc.getLength(), m_defaultStyle, true);
+        super.setText(text);
+        setCaretPosition(0);
+    }
+
+    private Style m_defaultStyle;
+}
+
+//----------------------------------------------------------------------------
+
+/** Implementation of GuiTextPane using a JTextArea for GCJ. */
+class GuiJTextArea
+    extends JTextArea
+    implements GuiTextPane
+{
+    public GuiJTextArea()
+    {
+    }
+
+    public void addStyle(String name, Color foreground, Color background)
+    {
+    }
+
+    public JComponent getComponent()
+    {
+        return this;
+    }
+
+    public String getSelection()
+    {
+        int start = getSelectionStart();
+        int end = getSelectionEnd();
+        Document doc = getDocument();
+        try
+        {
+            return doc.getText(start, end - start);
+        }
+        catch (BadLocationException e)
+        {
+            assert(false);
+            return "";
+        }   
+    }
+
+    public void markAll(Pattern pattern)
+    {
+    }
+
+    public void setTabFocusKeys()
+    {
+        // The implementation like in GuiJTextPane should work, but
+        // doesn't work with GCJ on FC4 (throws runtime exception)
+    }
+
+    public void setText(String text)
+    {
+        super.setText(text);
+        setCaretPosition(0);
+    }
+}
+
+//----------------------------------------------------------------------------
