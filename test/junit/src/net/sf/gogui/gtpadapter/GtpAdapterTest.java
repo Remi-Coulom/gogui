599767e621fe825739a4c6260b61cd903cd422cd
Markus Enzenberger <enz@users.sourceforge.net>
2005-08-15 22:36:13 +0000

    Copy sgf resource file to temporary file

--- test/junit/src/net/sf/gogui/gtpadapter/GtpAdapterTest.java
+++ test/junit/src/net/sf/gogui/gtpadapter/GtpAdapterTest.java
@@ -5,11 +5,16 @@
 
 package net.sf.gogui.gtpadapter;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.io.IOException;
 import net.sf.gogui.gtp.GtpClient;
 import net.sf.gogui.gtp.GtpError;
 import net.sf.gogui.gtp.GtpEngineConnection;
 import net.sf.gogui.gtp.GtpExpectEngine;
+import net.sf.gogui.utils.ErrorMessage;
+import net.sf.gogui.utils.StreamCopy;
 
 //----------------------------------------------------------------------------
 
@@ -26,7 +31,7 @@ public class GtpAdapterTest
         return new junit.framework.TestSuite(GtpAdapterTest.class);
     }
 
-    public void testLoadSgf() throws IOException, GtpError
+    public void testLoadSgf() throws ErrorMessage, IOException, GtpError
     {
         GtpExpectEngine expect = new GtpExpectEngine(null);
         GtpEngineConnection expectConnection
@@ -43,8 +48,21 @@ public class GtpAdapterTest
         expect.expect("clear_board", "");
         expect.expect("play B D4", "");
         expect.expect("play W Q16", "");
-        gtp.sendCommand("loadsgf test.sgf");
+        gtp.sendCommand("loadsgf " + getTmpFile("test.sgf").toString());
         assertTrue(expect.isExpectQueueEmpty());
+    }
+
+    private File getTmpFile(String name) throws ErrorMessage, IOException
+    {
+        InputStream in = getClass().getResourceAsStream(name);
+        if (in == null)
+            throw new ErrorMessage("Resource " + name + " not found");
+        File file = File.createTempFile("gogui", null);
+        file.deleteOnExit();
+        StreamCopy copy
+            = new StreamCopy(false, in, new FileOutputStream(file), true);
+        copy.run();
+        return file;
     }
 }
 
