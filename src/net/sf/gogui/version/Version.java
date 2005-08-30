2037c1eba15c6825e897e655ede06d30ec2dba4a
Markus Enzenberger <enz@users.sourceforge.net>
2005-08-30 21:06:36 +0000

    Make class final; add comment

--- src/net/sf/gogui/version/Version.java
+++ src/net/sf/gogui/version/Version.java
@@ -10,6 +10,10 @@ package net.sf.gogui.version;
 /** Version information for all packages. */
 public final class Version
 {
+    /** Get version for packages in net.sf.gogui.
+        @return Version string, no assumptions about the format should be
+        made.
+    */
     public static String get()
     {
         return "0.8pre1.x";
