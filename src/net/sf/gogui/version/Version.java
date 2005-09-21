1a95277002d0977ddbb64355d6dcd3ca6869734c
Markus Enzenberger <enz@users.sourceforge.net>
2005-09-21 17:32:40 +0000

    Change version to 0.8pre2.x

--- src/net/sf/gogui/version/Version.java
+++ src/net/sf/gogui/version/Version.java
@@ -16,7 +16,7 @@ public final class Version
     */
     public static String get()
     {
-        return "0.8pre2";
+        return "0.8pre2.x";
     }
 
     /** Make constructor unavailable; class is for namespace only. */
