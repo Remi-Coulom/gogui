adbc4d1171591584e6f91ab411a798d5fa68d555
Markus Enzenberger <enz@users.sourceforge.net>
2005-10-07 20:06:14 +0000

    Add GameTreeTest

---
+++ test/junit/src/net/sf/gogui/game/GameTreeTest.java
@@ -0,0 +1,43 @@
+//----------------------------------------------------------------------------
+// $Id$
+// $Source$
+//----------------------------------------------------------------------------
+
+package net.sf.gogui.game;
+
+//----------------------------------------------------------------------------
+
+public class GameTreeTest
+    extends junit.framework.TestCase
+{
+    public static void main(String args[])
+    {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static junit.framework.Test suite()
+    {
+        return new junit.framework.TestSuite(GameTreeTest.class);
+    }
+
+    public void testHasVariations()
+    {
+        GameTree tree = new GameTree();
+        Node root = tree.getRoot();
+        Node node1 = appendNewNode(root);
+        Node node2 = appendNewNode(node1);
+        Node node3 = appendNewNode(node2);
+        assertFalse(tree.hasVariations());
+        Node node4 = appendNewNode(node1);
+        assertTrue(tree.hasVariations());
+    }
+
+    private static Node appendNewNode(Node father)
+    {
+        Node child = new Node();
+        father.append(child);
+        return child;
+    }
+}
+
+//----------------------------------------------------------------------------
