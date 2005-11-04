fe0ffb6c2a8ceabf6acc2a58ca1bab2367c6acb1
Markus Enzenberger <enz@users.sourceforge.net>
2005-11-04 18:55:50 +0000

    Use Move.createPass

--- src/net/sf/gogui/go/MoveUtils.java
+++ src/net/sf/gogui/go/MoveUtils.java
@@ -26,7 +26,7 @@ public class MoveUtils
         {
             Move move = (Move)moves.get(i);
             if (move.getColor() != toMove)
-                result.add(Move.create(null, toMove));
+                result.add(Move.createPass(toMove));
             result.add(move);
             toMove = move.getColor().otherColor();
         }
