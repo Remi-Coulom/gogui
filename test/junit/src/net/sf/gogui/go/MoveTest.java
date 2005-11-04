7ad82d45ee5fb3f3aeccbafe1660d6b254bc83fb
Markus Enzenberger <enz@users.sourceforge.net>
2005-11-04 21:28:29 +0000

    Add testPass

--- test/junit/src/net/sf/gogui/go/MoveTest.java
+++ test/junit/src/net/sf/gogui/go/MoveTest.java
@@ -20,6 +20,18 @@ public class MoveTest
         return new junit.framework.TestSuite(MoveTest.class);
     }
 
+    public void testPass()
+    {
+        Move blackPass = Move.createPass(GoColor.BLACK);
+        assertNull(blackPass.getPoint());
+        assertSame(blackPass.getColor(), GoColor.BLACK);
+        assertSame(Move.create(null, GoColor.BLACK), blackPass);
+        Move whitePass = Move.createPass(GoColor.WHITE);
+        assertNull(whitePass.getPoint());
+        assertSame(whitePass.getColor(), GoColor.WHITE);
+        assertSame(Move.create(null, GoColor.WHITE), whitePass);
+    }
+
     public void testToString()
     {
         assertEquals("black A1", Move.create(0, 0, GoColor.BLACK).toString());
