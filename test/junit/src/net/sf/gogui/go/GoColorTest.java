655a5e34c21104a42301cdb758663e5e1abc06d3
Markus Enzenberger <enz@users.sourceforge.net>
2005-11-04 22:38:31 +0000

    Add GoColorTest

---
+++ test/junit/src/net/sf/gogui/go/GoColorTest.java
@@ -0,0 +1,38 @@
+//----------------------------------------------------------------------------
+// $Id$
+// $Source$
+//----------------------------------------------------------------------------
+
+package net.sf.gogui.go;
+
+//----------------------------------------------------------------------------
+
+public class GoColorTest
+    extends junit.framework.TestCase
+{
+    public static void main(String args[])
+    {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static junit.framework.Test suite()
+    {
+        return new junit.framework.TestSuite(GoColorTest.class);
+    }
+
+    public void testOtherColor()
+    {
+        assertSame(GoColor.BLACK.otherColor(), GoColor.WHITE);
+        assertSame(GoColor.WHITE.otherColor(), GoColor.BLACK);
+        assertSame(GoColor.EMPTY.otherColor(), GoColor.EMPTY);
+    }
+
+    public void testToString()
+    {
+        assertEquals(GoColor.BLACK.toString(), "black");
+        assertEquals(GoColor.WHITE.toString(), "white");
+        assertEquals(GoColor.EMPTY.toString(), "empty");
+    }
+}
+
+//----------------------------------------------------------------------------
