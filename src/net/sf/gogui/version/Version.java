af454ace5d53121d66808f3668996818d851b87f
Markus Enzenberger <enz@users.sourceforge.net>
2005-11-26 17:33:55 +0000

    Change version to 0.8.x

--- src/net/sf/gogui/version/Version.java
+++ src/net/sf/gogui/version/Version.java
@@ -16,7 +16,7 @@ public final class Version
     */
     public static String get()
     {
-        return "0.8";
+        return "0.8.x";
     }
 
     /** Make constructor unavailable; class is for namespace only. */
