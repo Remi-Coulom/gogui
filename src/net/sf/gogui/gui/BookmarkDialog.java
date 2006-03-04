f548abfc7cbb05bc21c7f8683814710619bfdfb5
Markus Enzenberger <enz@users.sourceforge.net>
2006-03-04 05:50:38 +0000

    Empty space between text fields

--- src/net/sf/gogui/gui/BookmarkDialog.java
+++ src/net/sf/gogui/gui/BookmarkDialog.java
@@ -54,9 +54,10 @@ public class BookmarkDialog
     public BookmarkDialog(Bookmark bookmark)
     {
         JPanel panel = new JPanel(new BorderLayout(GuiUtils.PAD, 0));
-        m_panelLeft = new JPanel(new GridLayout(0, 1, 0, 0));
+        m_panelLeft = new JPanel(new GridLayout(0, 1, 0, GuiUtils.SMALL_PAD));
         panel.add(m_panelLeft, BorderLayout.WEST);
-        m_panelRight = new JPanel(new GridLayout(0, 1, 0, 0));
+        m_panelRight =
+            new JPanel(new GridLayout(0, 1, 0, GuiUtils.SMALL_PAD));
         panel.add(m_panelRight, BorderLayout.CENTER);
         m_name = createEntry("Name:", bookmark.m_name);
         String file = "";
