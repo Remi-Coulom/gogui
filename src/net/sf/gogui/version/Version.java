5421793705d6b8817e5fe5b10450e92e908a59a4
Markus Enzenberger <enz@users.sourceforge.net>
2006-03-22 18:19:13 +0000

    Prepare for version 0.9pre2

--- src/net/sf/gogui/version/Version.java
+++ src/net/sf/gogui/version/Version.java
@@ -16,7 +16,7 @@ public final class Version
     */
     public static String get()
     {
-        return "0.9pre1.x";
+        return "0.9pre2";
     }
 
     /** Make constructor unavailable; class is for namespace only. */
