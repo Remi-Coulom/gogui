26499472aca5ce54e1fc0d5dc14f95a42109861f
Markus Enzenberger <enz@users.sourceforge.net>
2006-03-22 17:24:52 +0000

    Enable FileChooser.readOnly

--- src/net/sf/gogui/gui/GuiUtils.java
+++ src/net/sf/gogui/gui/GuiUtils.java
@@ -122,6 +122,11 @@ public class GuiUtils
     */
     public static void initLookAndFeel(String lookAndFeel)
     {
+        // Disables renaming mode in Sun Java 1.5, which makes using the
+        // JFileChooser a pain and is also enabled for open dialogs
+        // for no good reason. Unfortunately that sacrifices the new directory
+        // button, which is usefule for save dialogs
+        UIManager.put("FileChooser.readOnly", Boolean.TRUE);
         if ("".equals(lookAndFeel))
             return;
         boolean showError = true;
