7dd07304438b2c43ddbca7bc09c607918a8e2146
Markus Enzenberger <enz@users.sourceforge.net>
2006-09-07 15:28:09 +0000

    Use test fixture

--- test/junit/src/net/sf/gogui/gtp/GtpSynchronizerTest.java
+++ test/junit/src/net/sf/gogui/gtp/GtpSynchronizerTest.java
@@ -26,45 +26,89 @@ public class GtpSynchronizerTest
         return new junit.framework.TestSuite(GtpSynchronizerTest.class);
     }
 
-    public void testBasic() throws IOException, GtpError
+    public void setUp() throws IOException, GtpError
     {
-        GtpExpectEngine expect = new GtpExpectEngine(null);
-        GtpEngineConnection connection = new GtpEngineConnection(expect);
-        GtpClient gtp = connection.getGtpClient();
-        expect.expect("list_commands", "undo");
-        gtp.querySupportedCommands();
-        assertTrue(expect.isExpectQueueEmpty());
-        GtpSynchronizer synchronizer = new GtpSynchronizer(gtp, null);
-        Board board = new Board(19);
-        expect.expect("boardsize 19", "");
-        expect.expect("clear_board", "");
-        synchronizer.synchronize(board);
-        assertTrue(expect.isExpectQueueEmpty());
-        board.play(GoPoint.get(3, 4), GoColor.BLACK);
-        expect.expect("play b D5", "");
-        synchronizer.synchronize(board);
-        assertTrue(expect.isExpectQueueEmpty());
-        board.play(GoPoint.get(4, 4), GoColor.BLACK);
-        board.play(GoPoint.get(5, 5), GoColor.WHITE);
-        board.play(null, GoColor.BLACK);
-        expect.expect("play b E5", "");
-        expect.expect("play w F6", "");
-        expect.expect("play b PASS", "");
-        synchronizer.synchronize(board);
-        assertTrue(expect.isExpectQueueEmpty());
-        board.undo();
-        expect.expect("undo", "");
-        synchronizer.synchronize(board);
-        assertTrue(expect.isExpectQueueEmpty());
-        board.undo(2);
-        board.play(GoPoint.get(5, 5), GoColor.WHITE);
-        board.play(GoPoint.get(4, 4), GoColor.BLACK);
-        expect.expect("undo", "");
-        expect.expect("undo", "");
-        expect.expect("play w F6", "");
-        expect.expect("play b E5", "");
-        synchronizer.synchronize(board);
-        assertTrue(expect.isExpectQueueEmpty());
+        m_expect = new GtpExpectEngine(null);
+        m_connection = new GtpEngineConnection(m_expect);
+        m_gtp = m_connection.getGtpClient();
+        m_board = new Board(19);
+        m_synchronizer = new GtpSynchronizer(m_gtp, null);
+    }
+
+    public void testBasic() throws GtpError
+    {
+        expect("list_commands", "undo");
+        m_gtp.querySupportedCommands();
+        assertExpectQueueEmpty();
+        expect("boardsize 19", "");
+        expect("clear_board", "");
+        synchronize();
+        assertExpectQueueEmpty();
+        play(GoPoint.get(3, 4), GoColor.BLACK);
+        expect("play b D5", "");
+        synchronize();
+        assertExpectQueueEmpty();
+        play(GoPoint.get(4, 4), GoColor.BLACK);
+        play(GoPoint.get(5, 5), GoColor.WHITE);
+        play(null, GoColor.BLACK);
+        expect("play b E5", "");
+        expect("play w F6", "");
+        expect("play b PASS", "");
+        synchronize();
+        assertExpectQueueEmpty();
+        undo();
+        expect("undo", "");
+        synchronize();
+        assertExpectQueueEmpty();
+        undo(2);
+        play(GoPoint.get(5, 5), GoColor.WHITE);
+        play(GoPoint.get(4, 4), GoColor.BLACK);
+        expect("undo", "");
+        expect("undo", "");
+        expect("play w F6", "");
+        expect("play b E5", "");
+        synchronize();
+        assertExpectQueueEmpty();
+    }
+
+    private Board m_board;
+
+    private GtpExpectEngine m_expect;
+
+    private GtpEngineConnection m_connection;
+
+    private GtpClient m_gtp;
+
+    private GtpSynchronizer m_synchronizer;
+
+    private void assertExpectQueueEmpty()
+    {
+        assertTrue(m_expect.isExpectQueueEmpty());
+    }
+
+    private void expect(String command, String response)
+    {
+        m_expect.expect(command, response);
+    }
+
+    private void play(GoPoint point, GoColor color)
+    {
+        m_board.play(point, color);
+    }
+
+    private void synchronize() throws GtpError
+    {
+        m_synchronizer.synchronize(m_board);
+    }
+
+    private void undo()
+    {
+        m_board.undo();
+    }
+
+    private void undo(int n)
+    {
+        m_board.undo(n);
     }
 }
 
