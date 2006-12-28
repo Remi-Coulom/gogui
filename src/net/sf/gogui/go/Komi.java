228b5ddd0b2b9e1fa470e47a741aec15e9af4b22
Markus Enzenberger <enz@users.sourceforge.net>
2006-12-28 19:03:59 +0000

    Avoid GCJ warning about  serialVersionUID

--- src/net/sf/gogui/go/Komi.java
+++ src/net/sf/gogui/go/Komi.java
@@ -23,6 +23,11 @@ public final class Komi
         {
             super("Invalid komi: " + s);
         }
+
+        /** Serial version to suppress compiler warning.
+            Contains a marker comment for serialver.sourceforge.net
+        */
+        private static final long serialVersionUID = 0L; // SUID
     }
 
     /** Constructor.
