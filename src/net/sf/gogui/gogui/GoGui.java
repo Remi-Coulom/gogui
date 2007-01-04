83695f6d401389f092a6536110c0182384874051
Markus Enzenberger <enz@users.sourceforge.net>
2007-01-04 02:09:28 +0000

    Replace empty one-line string response by string: (empty response)

--- src/net/sf/gogui/gogui/GoGui.java
+++ src/net/sf/gogui/gogui/GoGui.java
@@ -1143,6 +1143,8 @@ public class GoGui
             {
                 if (response.indexOf("\n") < 0)
                 {
+                    if (response.trim().equals(""))
+                        response = "(empty response)";
                     showStatus(title + ": " + response);
                     statusContainsResponse = true;
                 }
