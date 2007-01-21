5b327b33320cfc7168a657400ef6b52df0112ed8
Markus Enzenberger <enz@users.sourceforge.net>
2007-01-21 03:22:58 +0000

    Improve icon

--- install.sh
+++ install.sh
@@ -74,12 +74,11 @@ install -m 644 doc/manual/man/*.1 $PREFIX/share/man/man1
 
 # Install icons
 
-for SIZE in 16 24 32 48 64 128; do
+for SIZE in 16 32 48 128; do
     install -d $PREFIX/share/icons/hicolor/"$SIZE"x"$SIZE"/apps
     install -m 644 src/net/sf/gogui/images/gogui-"$SIZE"x"$SIZE".png \
         $PREFIX/share/icons/hicolor/"$SIZE"x"$SIZE"/apps/gogui.png
 done
-
 # hicolor is the standard according to freedesktop.org, but for compatibility
 # we also install the icon to pixmaps
 install -d $PREFIX/share/pixmaps
