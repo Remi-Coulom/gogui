d3941360d17ceb18d432f5afc0e4dd078500f9bc
Markus Enzenberger <enz@users.sourceforge.net>
2007-01-27 16:52:42 +0000

    Remove unused import

--- src/net/sf/gogui/gui/GameInfo.java
+++ src/net/sf/gogui/gui/GameInfo.java
@@ -6,7 +6,6 @@ package net.sf.gogui.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
-import java.awt.Dimension;
 import java.awt.GridLayout;
 import javax.swing.Box;
 import javax.swing.Icon;
