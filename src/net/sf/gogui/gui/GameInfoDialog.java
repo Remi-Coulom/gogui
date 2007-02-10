cf2dae5d7a4da2865b42d39bccd0b9c4a8904a9d
Markus Enzenberger <enz@users.sourceforge.net>
2007-02-10 15:34:46 +0000

    Add serialVersionUID

--- src/net/sf/gogui/gui/GameInfoDialog.java
+++ src/net/sf/gogui/gui/GameInfoDialog.java
@@ -380,6 +380,11 @@ class TimeField
         return true;
     }
 
+    /** Serial version to suppress compiler warning.
+        Contains a marker comment for serialver.sourceforge.net
+    */
+    private static final long serialVersionUID = 0L; // SUID
+
     private JTextField m_textField;
 
     private JComboBox m_comboBox;
