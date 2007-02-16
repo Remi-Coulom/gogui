c2893e332a82b921e4b05a9531acbb2b3c6108f6
Markus Enzenberger <enz@users.sourceforge.net>
2007-02-16 21:28:53 +0000

    Add serialVersionUID

--- src/net/sf/gogui/gtp/GtpClient.java
+++ src/net/sf/gogui/gtp/GtpClient.java
@@ -49,6 +49,11 @@ public final class GtpClient
             super(e.getMessage());
             m_program = program;
         }        
+
+        /** Serial version to suppress compiler warning.
+            Contains a marker comment for serialver.sourceforge.net
+        */
+        private static final long serialVersionUID = 0L; // SUID
     }
 
     /** Callback if a timeout occured. */
