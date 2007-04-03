247c3ffe326ad358a768731f99e2e5962181f679
Markus Enzenberger <enz@users.sourceforge.net>
2007-04-03 21:05:34 +0000

    Add serialVersionUID

--- src/net/sf/gogui/gogui/GoGuiMenuBar.java
+++ src/net/sf/gogui/gogui/GoGuiMenuBar.java
@@ -187,6 +187,11 @@ public class GoGuiMenuBar
         m_menuViewShell.setEnabled(isShellShown);
     }
 
+    /** Serial version to suppress compiler warning.
+        Contains a marker comment for serialver.sourceforge.net
+    */
+    private static final long serialVersionUID = 0L; // SUID
+
     private final Listener m_listener;
 
     private final MenuChecked m_menuBookmarks;
