ebc59138184b01270f1d3ee09cb9ddc83a46ff47
Markus Enzenberger <enz@users.sourceforge.net>
2007-04-19 22:56:51 +0000

    Add test and documentation for PointList.remove()

--- src/net/sf/gogui/go/PointList.java
+++ src/net/sf/gogui/go/PointList.java
@@ -6,41 +6,54 @@ package net.sf.gogui.go;
 
 import java.util.ArrayList;
 
-/** List containing points. */
+/** List containing a points. */
 public final class PointList
     implements ConstPointList
 {
+    /** Construct empty point list. */
     public PointList()
     {
         this(0);
     }
 
+    /** Construct empty point list with initial capacity.
+        @param initialCapacity The number of points to reserve memory for.
+    */
     public PointList(int initialCapacity)
     {
         m_list = new ArrayList(initialCapacity);
     }
 
+    /** Construct point list with a single element.
+        @param p The initial point element.
+    */
     public PointList(GoPoint p)
     {
         this(1);
         add(p);
     }
 
+    /** Construct point list as a copy of another point list.
+        @param list The list to copy the points from.
+    */
     public PointList(ConstPointList list)
     {
         m_list = new ArrayList(((PointList)list).m_list);
     }
 
+    /** Add point at the end of the list. */
     public void add(GoPoint p)
     {
         m_list.add(p);
     }
 
+    /** Add points of another list  at the end of this list. */
     public void addAll(ConstPointList list)
     {
         m_list.addAll(((PointList)list).m_list);
     }
 
+    /** Remove all points from the list. */
     public void clear()
     {
         m_list.clear();
@@ -59,12 +72,13 @@ public final class PointList
         return list.m_list.equals(m_list);
     }
 
+    /** Get the point at the specified position. */
     public GoPoint get(int index)
     {
         return (GoPoint)m_list.get(index);
     }
 
-    /** Get an empty const point list.
+    /** Get an empty constant point list.
         Can be used at places where an empty temporary point list is needed
         that is never modified to avoid memory allocation.
     */
@@ -83,6 +97,12 @@ public final class PointList
         return m_list.isEmpty();
     }
 
+    /** Remove first occurence of a point from the list.
+        Does not change the order of the remaining elements.
+        Does nothing if point is not in the list.
+        @param p The point to remove.
+        @return <code>true</code>, if the point was found and removed.
+    */
     public boolean remove(GoPoint p)
     {
         return m_list.remove(p);
--- test/junit/src/net/sf/gogui/go/PointListTest.java
+++ test/junit/src/net/sf/gogui/go/PointListTest.java
@@ -36,6 +36,35 @@ public final class PointListTest
         assertTrue(list1.hashCode() != list3.hashCode());
     }
 
+    public void testRemove()
+    {
+        PointList list = new PointList();
+        GoPoint p1 = GoPoint.get(1, 1);
+        GoPoint p2 = GoPoint.get(2, 2);
+        GoPoint p3 = GoPoint.get(3, 3);
+        list.add(p1);
+        list.add(p2);
+        list.add(p1);
+        list.add(p3);
+        boolean result = list.remove(p1);
+        assertTrue(result);
+        assertEquals(3, list.size());
+        assertEquals(p2, list.get(0));
+        assertEquals(p1, list.get(1));
+        assertEquals(p3, list.get(2));
+        result = list.remove(p1);
+        assertTrue(result);
+        assertEquals(2, list.size());
+        assertEquals(p2, list.get(0));
+        assertEquals(p3, list.get(1));
+        result = list.remove(p3);
+        assertTrue(result);
+        assertEquals(1, list.size());
+        assertEquals(p2, list.get(0));
+        result = list.remove(p1);
+        assertFalse(result);
+    }
+
     public void testToString()
     {
         assertEquals("", PointList.toString(null));
@@ -47,4 +76,3 @@ public final class PointListTest
         assertEquals("A1 B2", list.toString());
     }
 }
-
