361f3c031fc518e975c01e138c7b7cbc261389dc
Markus Enzenberger <enz@users.sourceforge.net>
2007-08-13 17:38:37 +0000

    Keep parameter dialog open if setting a new parameter value fails

--- src/net/sf/gogui/gui/ParameterDialog.java
+++ src/net/sf/gogui/gui/ParameterDialog.java
@@ -7,19 +7,27 @@ package net.sf.gogui.gui;
 import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.Frame;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
 import javax.swing.Box;
+import javax.swing.JDialog;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import static javax.swing.JOptionPane.OK_CANCEL_OPTION;
+import static javax.swing.JOptionPane.PLAIN_MESSAGE;
+import static javax.swing.JOptionPane.UNINITIALIZED_VALUE;
+import static javax.swing.JOptionPane.VALUE_PROPERTY;
 import javax.swing.JPanel;
 import javax.swing.JSeparator;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import net.sf.gogui.gtp.GtpError;
+import net.sf.gogui.util.ObjectUtil;
 import net.sf.gogui.util.StringUtil;
 
 /** Dialog for editing parameters in response to an analyze command of type
@@ -27,76 +35,67 @@ import net.sf.gogui.util.StringUtil;
 */
 public class ParameterDialog
 {
-    public static void editParameters(String paramCommand, Frame owner,
+    public static void editParameters(final String paramCommand, Frame owner,
                                       String title, String response,
-                                      GuiGtpClient gtp,
-                                      MessageDialogs messageDialogs)
+                                      final GuiGtpClient gtp,
+                                      final MessageDialogs messageDialogs)
     {
-        ArrayList<Parameter> parameters = parseResponse(response);
-        int numberParameters = parameters.size();
-        Box outerBox = Box.createHorizontalBox();
-        int i = 0;
-        int numberColumns = 0;
-        Box box = null;
-        while (i < numberParameters)
-        {
-            if (i % 30 == 0)
-            {
-                if (box != null)
-                {
-                    if (numberColumns > 0)
+        final ArrayList<Parameter> parameters = parseResponse(response);
+        Component mainComponent = createMainComponent(parameters);
+        final Object options[] = { "Ok", "Cancel" };
+        final JOptionPane optionPane =
+            new JOptionPane(mainComponent, PLAIN_MESSAGE, OK_CANCEL_OPTION,
+                            null, options, options[0]);
+        final JDialog dialog = new JDialog(owner, title, true);
+        dialog.setContentPane(optionPane);
+        optionPane.addPropertyChangeListener(new PropertyChangeListener() {
+                public void propertyChange(PropertyChangeEvent event) {
+                    String prop = event.getPropertyName();
+                    if (dialog.isVisible() && event.getSource() == optionPane
+                        && prop.equals(VALUE_PROPERTY))
                     {
-                        outerBox.add(GuiUtil.createFiller());
-                        outerBox.add(new JSeparator(SwingConstants.VERTICAL));
-                        outerBox.add(GuiUtil.createFiller());
+                        Object value = optionPane.getValue();
+                        if (ObjectUtil.equals(value, UNINITIALIZED_VALUE))
+                            return;
+                        if (ObjectUtil.equals(value, options[0]))
+                        {
+                            for (int i = 0; i < parameters.size(); ++i)
+                            {
+                                Parameter parameter = parameters.get(i);
+                                if (! parameter.isChanged())
+                                    continue;
+                                String key = parameter.getKey();
+                                String newValue = parameter.getNewValue();
+                                String command =
+                                    paramCommand + " " + key + " " + newValue;
+                                try
+                                {
+                                    gtp.send(command);
+                                }
+                                catch (GtpError e)
+                                {
+                                    Component component =
+                                        parameter.getComponent();
+                                    String mainMessage =
+                                        "Could not change parameter " + key;
+                                    String optionalMessage =
+                                        StringUtil.capitalize(e.getMessage());
+                                    messageDialogs.showError(dialog,
+                                                             mainMessage,
+                                                             optionalMessage);
+                                    optionPane.setValue(UNINITIALIZED_VALUE);
+                                    component.requestFocus();
+                                    return;
+                                }
+                            }
+                        }
+                        dialog.setVisible(false);
                     }
-                    outerBox.add(box);
-                    ++numberColumns;
                 }
-                box = Box.createVerticalBox();
-            }
-            box.add((parameters.get(i)).getComponent());
-            ++i;
-        }
-        if (box != null)
-        {
-            if (numberColumns > 0)
-            {
-                outerBox.add(GuiUtil.createFiller());
-                outerBox.add(new JSeparator(SwingConstants.VERTICAL));
-                outerBox.add(GuiUtil.createFiller());
-            }
-            outerBox.add(box);
-        }
-        Object options[] = { "Ok", "Cancel" };
-        int r =
-            JOptionPane.showOptionDialog(owner, outerBox, title,
-                                         JOptionPane.OK_CANCEL_OPTION,
-                                         JOptionPane.PLAIN_MESSAGE, null,
-                                         options, options[0]);
-        if (r != 0)
-            return;
-        for (i = 0; i < parameters.size(); ++i)
-        {
-            Parameter parameter = parameters.get(i);
-            if (! parameter.isChanged())
-                continue;
-            String key = parameter.getKey();
-            String newValue = parameter.getNewValue();
-            String command = paramCommand + " " + key + " " + newValue;
-            try
-            {
-                gtp.send(command);
-            }
-            catch (GtpError e)
-            {
-                messageDialogs.showError(owner,
-                                        "Could not change parameter "
-                                        + parameter.getKey(),
-                                        StringUtil.capitalize(e.getMessage())
-                                        );
-            }
-        }
+            });
+        dialog.pack();
+        dialog.setLocationRelativeTo(owner);
+        dialog.setVisible(true);
     }
 
     private abstract static class Parameter
@@ -280,5 +279,46 @@ public class ParameterDialog
                 parameters.add(new StringParameter(key, value));
         }
         return parameters;
+    }
+
+    private static Component
+        createMainComponent(ArrayList<Parameter> parameters)
+    {
+        int numberParameters = parameters.size();
+        Box outerBox = Box.createHorizontalBox();
+        int i = 0;
+        int numberColumns = 0;
+        Box box = null;
+        while (i < numberParameters)
+        {
+            if (i % 30 == 0)
+            {
+                if (box != null)
+                {
+                    if (numberColumns > 0)
+                    {
+                        outerBox.add(GuiUtil.createFiller());
+                        outerBox.add(new JSeparator(SwingConstants.VERTICAL));
+                        outerBox.add(GuiUtil.createFiller());
+                    }
+                    outerBox.add(box);
+                    ++numberColumns;
+                }
+                box = Box.createVerticalBox();
+            }
+            box.add((parameters.get(i)).getComponent());
+            ++i;
+        }
+        if (box != null)
+        {
+            if (numberColumns > 0)
+            {
+                outerBox.add(GuiUtil.createFiller());
+                outerBox.add(new JSeparator(SwingConstants.VERTICAL));
+                outerBox.add(GuiUtil.createFiller());
+            }
+            outerBox.add(box);
+        }
+        return outerBox;
     }
 }
