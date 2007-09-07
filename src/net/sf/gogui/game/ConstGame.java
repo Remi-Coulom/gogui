ba4e448caa6150b720dc509bb3aff3438bcb263d
Markus Enzenberger <enz@users.sourceforge.net>
2007-09-07 21:20:55 +0000

    Add getGameInformationNode()

--- src/net/sf/gogui/game/ConstGame.java
+++ src/net/sf/gogui/game/ConstGame.java
@@ -20,6 +20,8 @@ public interface ConstGame
 
     ConstGameInformation getGameInformation(ConstNode node);
 
+    ConstNode getGameInformationNode();
+
     int getMoveNumber();
 
     ConstNode getRoot();
