03657d2a1842debe835ddf00fdac70da19f1ec92
Markus Enzenberger <enz@users.sourceforge.net>
2007-10-12 16:44:58 +0000

    Fix bug in HtmlUtil.escape

---
+++ test/junit/src/net/sf/gogui/util/HtmlUtilTest.java
@@ -0,0 +1,25 @@
+//----------------------------------------------------------------------------
+// FileUtilTest.java
+//----------------------------------------------------------------------------
+
+package net.sf.gogui.util;
+
+public final class HtmlUtilTest
+    extends junit.framework.TestCase
+{
+    public static void main(String args[])
+    {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static junit.framework.Test suite()
+    {
+        return new junit.framework.TestSuite(HtmlUtilTest.class);
+    }
+
+    public void testEscape()
+    {
+        assertEquals("&lt;ok&gt;&amp;&quot;&apos;",
+                     HtmlUtil.escape("<ok>&\"'"));
+    }
+}
