60a063f037281bab9dd2fe558c1cf54c2a601826
Markus Enzenberger <enz@users.sourceforge.net>
2007-10-19 02:37:43 +0000

    Add option -check

--- src/net/sf/gogui/tools/convert/Main.java
+++ src/net/sf/gogui/tools/convert/Main.java
@@ -28,6 +28,7 @@ public final class Main
         try
         {
             String options[] = {
+                "check",
                 "config:",
                 "force",
                 "format:",
@@ -48,44 +49,53 @@ public final class Main
             }
             boolean force = opt.contains("force");
             String title = opt.get("title", "");
+            boolean checkOnly = opt.contains("check");
             ArrayList<String> arguments = opt.getArguments();
-            if (arguments.size() != 2)
+            if (! (arguments.size() == 2
+                   || (arguments.size() == 1 && checkOnly)))
             {
                 printUsage(System.err);
                 System.exit(-1);
             }
             File in = new File(arguments.get(0));
-            File out = new File(arguments.get(1));
-            String format;
-            if (opt.contains("format"))
-                format = opt.get("format");
-            else
-                format =
-                    FileUtil.getExtension(out).toLowerCase(Locale.ENGLISH);
-            if (! format.equals("sgf")
-                && ! format.equals("tex")
-                && ! format.equals("xml"))
-                throw new Exception("Unknown format");
+            File out = null;
+            String format = null;
+            if (! checkOnly)
+            {
+                out = new File(arguments.get(1));
+                if (opt.contains("format"))
+                    format = opt.get("format");
+                else
+                    format =
+                        FileUtil.getExtension(out).toLowerCase(Locale.ENGLISH);
+                if (! format.equals("sgf")
+                    && ! format.equals("tex")
+                    && ! format.equals("xml"))
+                    throw new Exception("Unknown format");
+                if (out.exists() && ! force)
+                    throw new Exception("File \"" + out + "\" already exists");
+            }
             if (! in.exists())
                 throw new Exception("File \"" + in + "\" not found");
-            if (out.exists() && ! force)
-                throw new Exception("File \"" + out + "\" already exists");
             GameReader reader = new GameReader(in);
             ConstGameTree tree = reader.getTree();
             String warnings = reader.getWarnings();
             if (warnings != null)
                 System.err.println(warnings);
-            String version = Version.get();
-            if (format.equals("xml"))
-                new XmlWriter(new FileOutputStream(out), tree,
-                              "GoGuiConvert:" + version);
-            else if (format.equals("sgf"))
-                new SgfWriter(new FileOutputStream(out), tree, "GoGuiConvert",
-                              version);
-            else if (format.equals("tex"))
-                new TexWriter(title, new FileOutputStream(out), tree);
-            else
-                assert false; // check above
+            if (! checkOnly)
+            {
+                String version = Version.get();
+                if (format.equals("xml"))
+                    new XmlWriter(new FileOutputStream(out), tree,
+                                  "GoGuiConvert:" + version);
+                else if (format.equals("sgf"))
+                    new SgfWriter(new FileOutputStream(out), tree,
+                                  "GoGuiConvert", version);
+                else if (format.equals("tex"))
+                    new TexWriter(title, new FileOutputStream(out), tree);
+                else
+                    assert false; // checked above
+            }
         }
         catch (Throwable t)
         {
