6e72324a5ce78c62b6e04de38ecd939e8cffda64
Markus Enzenberger <enz@users.sourceforge.net>
2007-11-01 21:40:34 +0000

    Don't use modifier for interrupt shortcut

--- src/net/sf/gogui/gogui/GoGuiActions.java
+++ src/net/sf/gogui/gogui/GoGuiActions.java
@@ -380,7 +380,7 @@ public class GoGuiActions
                 m_goGui.actionImportTextPositionFromClipboard(); } };
 
     public final Action m_actionInterrupt =
-        new Action("Interrupt", null, KeyEvent.VK_ESCAPE, SHORTCUT,
+        new Action("Interrupt", null, KeyEvent.VK_ESCAPE, 0,
                    "gogui-interrupt") {
             public void actionPerformed(ActionEvent e) {
                 m_goGui.actionInterrupt(); } };
