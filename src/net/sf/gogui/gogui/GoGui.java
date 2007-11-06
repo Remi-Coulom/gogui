756fb7371c9cc176c71e4782d48320e5a6eda20d
Markus Enzenberger <enz@users.sourceforge.net>
2007-11-06 20:57:28 +0000

    Restore hand cursor if analyze command with point args

--- src/net/sf/gogui/gogui/GoGui.java
+++ src/net/sf/gogui/gogui/GoGui.java
@@ -2897,7 +2897,7 @@ public class GoGui
     private boolean endLengthyCommand(boolean isCritical,
                                       boolean showError)
     {
-        setBoardCursor(Cursor.DEFAULT_CURSOR);
+        restoreBoardCursor();
         clearStatus();
         if (m_shell != null)
             m_shell.setCommandInProgess(false);
@@ -3439,6 +3439,16 @@ public class GoGui
         m_guiBoard.clearAll();
         updateFromGoBoard();
         updateGuiBoard();
+    }
+
+    private void restoreBoardCursor()
+    {
+        if (m_analyzeCommand != null
+            && (m_analyzeCommand.needsPointArg()
+                || m_analyzeCommand.needsPointListArg()))
+            setBoardCursor(Cursor.HAND_CURSOR);
+        else
+            setBoardCursorDefault();
     }
 
     private void restoreLocation(JDialog dialog, String name)
