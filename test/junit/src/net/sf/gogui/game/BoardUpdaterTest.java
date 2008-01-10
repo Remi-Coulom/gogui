89a12460ce23c50caba2d66ef6dd35ec90d7db41
Markus Enzenberger <enz@users.sourceforge.net>
2008-01-10 18:06:37 +0000

    Change image

--- test/junit/src/net/sf/gogui/game/BoardUpdaterTest.java
+++ test/junit/src/net/sf/gogui/game/BoardUpdaterTest.java
@@ -7,6 +7,7 @@ package net.sf.gogui.game;
 import net.sf.gogui.go.Board;
 import net.sf.gogui.go.GoColor;
 import net.sf.gogui.go.GoPoint;
+import net.sf.gogui.go.Move;
 
 public final class BoardUpdaterTest
     extends junit.framework.TestCase
@@ -56,6 +57,29 @@ public final class BoardUpdaterTest
         assertEquals(GoColor.WHITE, board.getColor(GoPoint.get(2, 4)));
         assertEquals(GoColor.WHITE, board.getColor(GoPoint.get(2, 5)));
         assertEquals(GoColor.BLACK, board.getColor(GoPoint.get(3, 1)));
+    }
+
+    /** Test that setting a stone on an point occupied by the second move
+        in a game works.
+        Checks for a bug that was only triggered, if the changed stone color
+        was the second move played, not the first move,
+    */
+    public void testSetupOnOccupiedMoveTwo()
+    {
+        GameTree tree = new GameTree();
+        Node root = tree.getRoot();
+        Node node1 = new Node();
+        root.append(node1);
+        node1.setMove(Move.get(GoColor.BLACK, GoPoint.get(4, 4)));
+        Node node2 = new Node();
+        node1.append(node2);
+        node2.setMove(Move.get(GoColor.WHITE, GoPoint.get(3, 3)));
+        Node node3 = new Node();
+        node2.append(node3);
+        node3.addStone(GoColor.BLACK, GoPoint.get(3, 3));
+        Board board = new Board(19);
+        m_updater.update(tree, node3, board);
+        assertEquals(GoColor.BLACK, board.getColor(GoPoint.get(3, 3)));
     }
 
     private BoardUpdater m_updater;
