373b0da0de67b920d9a374e082611aea828bd67d
Markus Enzenberger <enz@users.sourceforge.net>
2008-03-17 20:46:38 +0000

    More internationalization

--- src/net/sf/gogui/gogui/GoGuiUtil.java
+++ src/net/sf/gogui/gogui/GoGuiUtil.java
@@ -4,12 +4,14 @@ package net.sf.gogui.gogui;
 
 import java.awt.Frame;
 import java.awt.Point;
+import static java.text.MessageFormat.format;
 import net.sf.gogui.game.ConstGame;
 import net.sf.gogui.game.ConstNode;
 import net.sf.gogui.game.NodeUtil;
 import net.sf.gogui.go.GoPoint;
 import net.sf.gogui.go.Move;
 import net.sf.gogui.go.PointList;
+import static net.sf.gogui.gogui.I18n.i18n;
 import net.sf.gogui.gtp.AnalyzeType;
 import net.sf.gogui.gtp.GtpUtil;
 import net.sf.gogui.gui.GuiBoard;
@@ -55,8 +57,8 @@ public final class GoGuiUtil
         int totalMoves = moveNumber + movesLeft;
         Move move = node.getMove();
         String variation = NodeUtil.getVariationString(node);
+        boolean mainVar = "".equals(variation);
         StringBuilder moveText = new StringBuilder(128);
-        StringBuilder toolTip = new StringBuilder(128);
         if (moveNumber > 0)
         {
             moveText.append(moveNumber);
@@ -74,45 +76,58 @@ public final class GoGuiUtil
             moveText.append(move);
             moveText.append(' ');
         }
-        if (! "".equals(variation))
+        if (! mainVar)
         {
             moveText.append('[');
             moveText.append(variation);
             moveText.append(']');
         }
-        if (move == null)
-        {
-            toolTip.append(moveNumber);
-            if (moveNumber == 1)
-                toolTip.append(" move played ");
-            else
-                toolTip.append(" moves played ");
-        }
-        else
-        {
-            toolTip.append("Last move ");
-            toolTip.append(moveNumber);
-            toolTip.append(' ');
-            toolTip.append(move);
-            toolTip.append(' ');
-        }
-        if (movesLeft > 0)
-        {
-            toolTip.append("of ");
-            toolTip.append(totalMoves);
-            if (totalMoves == 1)
-                toolTip.append(" move ");
-            else
-                toolTip.append(" moves ");
-        }
-        if ("".equals(variation))
-            toolTip.append("in main variation");
-        else
-        {
-            toolTip.append("in variation ");
-            toolTip.append(variation);
-        }
-        statusBar.setMoveText(moveText.toString(), toolTip.toString());
+        String tip = null;
+        boolean lastMove = (move != null);
+        boolean noLastMove1 = (move == null && moveNumber == 1);
+        boolean noLastMoveN = (move == null && moveNumber > 1);
+        boolean noMovesLeft = (movesLeft == 0);
+        boolean movesLeft1 = (movesLeft > 0 && totalMoves == 1);
+        boolean movesLeftN = (movesLeft > 0 && totalMoves > 1);
+        if (noLastMove1 && noMovesLeft && mainVar)
+            tip = i18n("TT_MOVETEXT_1");
+        else if (noLastMoveN && noMovesLeft && mainVar)
+            tip = format(i18n("TT_MOVETEXT_2"), moveNumber);
+        else if (lastMove && noMovesLeft && mainVar)
+            tip = format(i18n("TT_MOVETEXT_3"), moveNumber, move);
+        else if (noLastMove1 && movesLeft1 && mainVar)
+            tip = i18n("TT_MOVETEXT_4");
+        else if (noLastMoveN && movesLeft1 && mainVar)
+            tip = format(i18n("TT_MOVETEXT_5"), moveNumber);
+        else if (lastMove && movesLeft1 && mainVar)
+            tip = format(i18n("TT_MOVETEXT_6"), moveNumber, move);
+        else if (noLastMove1 && movesLeftN && mainVar)
+            tip = format(i18n("TT_MOVETEXT_7"), totalMoves);
+        else if (noLastMoveN && movesLeftN && mainVar)
+            tip = format(i18n("TT_MOVETEXT_8"), moveNumber, totalMoves);
+        else if (lastMove && movesLeftN && mainVar)
+            tip = format(i18n("TT_MOVETEXT_9"), moveNumber, move, totalMoves);
+        else if (noLastMove1 && noMovesLeft && ! mainVar)
+            tip = format(i18n("TT_MOVETEXT_10"), variation);
+        else if (noLastMoveN && noMovesLeft && ! mainVar)
+            tip = format(i18n("TT_MOVETEXT_11"), moveNumber, variation);
+        else if (lastMove && noMovesLeft && ! mainVar)
+            tip = format(i18n("TT_MOVETEXT_12"), moveNumber, move, variation);
+        else if (noLastMove1 && movesLeft1 && ! mainVar)
+            tip = format(i18n("TT_MOVETEXT_13"), variation);
+        else if (noLastMoveN && movesLeft1 && ! mainVar)
+            tip = format(i18n("TT_MOVETEXT_14"), moveNumber, variation);
+        else if (lastMove && movesLeft1 && ! mainVar)
+            tip = format(i18n("TT_MOVETEXT_15"), moveNumber, move, variation);
+        else if (noLastMove1 && movesLeftN && ! mainVar)
+            tip = format(i18n("TT_MOVETEXT_16"), totalMoves, variation);
+        else if (noLastMoveN && movesLeftN && ! mainVar)
+            tip = format(i18n("TT_MOVETEXT_17"), moveNumber, totalMoves,
+                         variation);
+        else if (lastMove && movesLeftN && ! mainVar)
+            tip = format(i18n("TT_MOVETEXT_18"), moveNumber, move, totalMoves,
+                         variation);
+        statusBar.setMoveText(moveText.toString(), tip);
     }
 
     private static class PointSelectionMarker
