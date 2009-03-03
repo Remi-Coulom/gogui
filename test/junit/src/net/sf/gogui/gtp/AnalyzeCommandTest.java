a247f751e4676ec7e3999dbf98e286484fa757c0
Markus Enzenberger <enz@users.sourceforge.net>
2009-03-03 16:29:36 +0000

    Add AnalyzeCommandTest

---
+++ test/junit/src/net/sf/gogui/gtp/AnalyzeCommandTest.java
@@ -0,0 +1,35 @@
+// AnalyzeCommandTest.java
+
+package net.sf.gogui.gtp;
+
+import static net.sf.gogui.go.GoColor.BLACK;
+
+public final class AnalyzeCommandTest
+    extends junit.framework.TestCase
+{
+    public static void main(String args[])
+    {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static junit.framework.Test suite()
+    {
+        return new junit.framework.TestSuite(AnalyzeCommandTest.class);
+    }
+
+    /** Test that replacement of string argument works if string contains
+        backslashes.
+    */
+    public void testStringArgWithBackslashes()
+    {
+        AnalyzeCommand cmd = createCommand("none/Test/test %s");
+        cmd.setStringArg("foo\\bar");
+        String line = cmd.replaceWildCards(BLACK);
+        assertEquals("test foo\\bar", line);
+    }
+
+    private static AnalyzeCommand createCommand(String definition)
+    {
+        return new AnalyzeCommand(new AnalyzeDefinition(definition));
+    }
+}
