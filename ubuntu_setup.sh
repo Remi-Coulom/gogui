472319da8b45c13bbcedce71a5333ce6efb53ec7
Rémi Coulom <remi.coulom@gmail.com>
2025-03-11 14:22:44 +0100

    modernize ubuntu_setup.sh

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-sudo apt install ant xsltproc docbook-xsl inkscape openjdk-11-jdk
+sudo apt install ant xsltproc docbook-xsl inkscape openjdk-21-jdk
 
 cd src/net/sf/gogui/images
 ./svg.sh
